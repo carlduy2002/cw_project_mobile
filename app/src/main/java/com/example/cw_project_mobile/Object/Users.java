3eb3d726280c76be4ba7158e23c40d13c340d357
mobile_app <duydhgcc200018@fpt.edu.vn>
2023-11-17 10:40:16 +0700

    update file code user 17/11/23

---
+++ app/src/main/java/com/example/cw_project_mobile/Object/Users.java
@@ -0,0 +1,89 @@
+package com.example.cw_project_mobile.Object;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+public class Users implements Parcelable {
+    String username, email, password, address;
+    int id;
+
+    public Users() {
+    }
+
+    protected Users(Parcel in) {
+        username = in.readString();
+        email = in.readString();
+        password = in.readString();
+        address = in.readString();
+        id = in.readInt();
+    }
+
+    public static final Creator<Users> CREATOR = new Creator<Users>() {
+        @Override
+        public Users createFromParcel(Parcel in) {
+            return new Users(in);
+        }
+
+        @Override
+        public Users[] newArray(int size) {
+            return new Users[size];
+        }
+    };
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+
+        dest.writeString(username);
+        dest.writeString(email);
+        dest.writeString(password);
+        dest.writeString(address);
+        dest.writeInt(id);
+    }
+}
